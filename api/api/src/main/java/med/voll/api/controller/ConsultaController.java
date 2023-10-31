f85bc25014d73185f3736d6e5e8476f114cea23a
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2023-10-31 13:59:05 -0300

    Adicionando a funcionalidade de cancelar consultas

--- api/api/src/main/java/med/voll/api/controller/ConsultaController.java
+++ api/api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,16 +1,11 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.domain.consulta.AgendaDeConsultas;
-import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
-import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import med.voll.api.domain.consulta.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
@@ -19,6 +14,9 @@ public class ConsultaController {
     @Autowired
     private AgendaDeConsultas agendaDeConsultas;
 
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
@@ -26,5 +24,12 @@ public class ConsultaController {
         agendaDeConsultas.agendar(dados);
 
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
+    }
+
+    @Transactional
+    @DeleteMapping
+    public ResponseEntity cancelarConseulta(@RequestBody @Valid DadosCancelamentoConsulta dados) {
+        agendaDeConsultas.cancelarConsulta(dados);
+        return  ResponseEntity.noContent().build();
     }
 }
--- api/api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ api/api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -30,7 +30,7 @@ public class AgendaDeConsultas {
 
         var medico = escolherMedico(dados);
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
-        var consulta = new Consulta(null, medico, paciente, dados.data());
+        var consulta = new Consulta(null, medico, paciente, dados.data(), null);
 
         consultaRepository.save(consulta);
     }
@@ -45,5 +45,14 @@ public class AgendaDeConsultas {
         }
 
         return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
+    }
+
+    public void cancelarConsulta(DadosCancelamentoConsulta dados){
+        if (!consultaRepository.existsById(dados.idConsulta())) {
+            throw  new ValidacaoExeption("Id da consulta informado não existe.");
+        }
+
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        consulta.cancelar(dados.motivo());
     }
 }
